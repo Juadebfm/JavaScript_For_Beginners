adfe5f12bd134e9f0e79f5f3c78b15da1cded002
Juadeb <juadebgabriel@gmail.com>
2023-05-07 22:15:18 +0100

    fix

---
+++ operators.js
@@ -0,0 +1,13 @@
+// There are different types of JavaScript operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// String Operators
+// Logical Operators
+// Bitwise Operators
+// Ternary Operators
+// Type Operators
+
+//Arithmetic
+
