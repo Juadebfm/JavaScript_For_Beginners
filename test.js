e6ada3ea7418d84799aa03c1d6b447f6d29567d3
Juadeb <juadebgabriel@gmail.com>
2023-05-20 10:56:51 +0100

    deleted

--- test.js
+++
@@ -1,7 +0,0 @@
-function greetings(name = "Peter") {
-    let message = `${name}, Pluralcode February Cohort`;
-    return message;
-  }
-  
-  console.log(greetings());
-  console.log(greetings("Pluracode"));
