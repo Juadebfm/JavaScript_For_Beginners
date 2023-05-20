ccb664f7c4f641bb256492e8e6bc1cb25326f568
Juadeb <juadebgabriel@gmail.com>
2023-05-20 05:54:57 +0100

    fix

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,10 @@
-//comment
+let age = prompt("Enter your age:");
+
+if (age >= 18) {
+  console.log("You are old enough to drive");
+} else if (age < 18) {
+  let noOfYears = 18 - age;
+  console.log(`You have to wait for ${noOfYears} before you can drive`);
+} else {
+    console.log("You are an alien")
+}
