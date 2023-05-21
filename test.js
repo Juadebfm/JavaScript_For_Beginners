61869d67f90d72c837122b7e69659a8d2db14bbf
Juadeb <juadebgabriel@gmail.com>
2023-05-21 18:46:06 +0100

    fix

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-let obj = {
-  firstname: "sdjkksjdfbk",
-};
-
-function Bird() {
-  this.name = "Albert";
-  this.color = "blue";
-  this.numLegs = 2;
-}
-
-let juadeb = new Bird();
-
-let now = new Date();
-now.get
+const  person = "jay"
