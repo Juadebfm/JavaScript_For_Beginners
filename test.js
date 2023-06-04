14ec3f5888e591918b84c6c8f2a1425fcca14dfd
Juadeb <juadebgabriel@gmail.com>
2023-06-04 10:11:16 +0100

    fix

--- test.js
+++ test.js
@@ -1,29 +1,26 @@
-const usersText = `{
-    "users":[
-      {
-        "firstName":"Julius",
-        "lastName":"Adebowale",
-        "age":250,
-        "email":"asab@asb.com"
-      },
-      {
-        "firstName":"Alex",
-        "lastName":"James",
-        "age":25,
-        "email":"alex@alex.com"
-      },
-      {
-      "firstName":"Lidiya",
-      "lastName":"Tekle",
-      "age":28,
-      "email":"lidiya@lidiya.com"
-      }
-    ]
-    }`;
+const myObj = {
+  name: "John",
+  age: 30,
+  hobbies: ["Eat", "Sleep", "Rest"],
+  hello: function () {
+    console.log("Hi!, I'm John");
+  },
+};
 
-const usersObj = JSON.parse(usersText, (key, value) => {
-  let newValue =
-    typeof value == "string" && key != "email" ? value.toUpperCase() : value;
-  return newValue;
-});
-console.log(usersObj);
+console.log(myObj);
+console.log(myObj.name);
+console.log(myObj.age);
+console.log(myObj.hobbies);
+myObj.hello();
+
+//convert myObj to json
+const myObjJsn = JSON.stringify(myObj);
+console.log(myObjJsn);
+//notice that JSON doesn't send options?!
+console.log(typeof myObjJsn);
+console.log(myObjJsn.name); //undefined
+
+// to convert JSON back to object
+const myObj2 = JSON.parse(myObjJsn);
+console.log(myObj2);
+//Note you will lose the method in your object
