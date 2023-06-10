26e65ed4f051716be8102b2339de62cbbb3a692d
Juadeb <juadebgabriel@gmail.com>
2023-06-10 11:48:29 +0100

    fix

--- test.js
+++ test.js
@@ -1,16 +1,38 @@
+//Promise Maker
 function getWeather() {
-    return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
       resolve("Sunny");
-    });
-  }
-  
-  function onSuccess(data) {
-    console.log(`Success: ${data}`);
-  }
-  
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-  
-  getWeather().then(onSuccess, onError);
-  
+    }, 1000);
+  });
+}
+
+function getWeatherIcon(weather) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      switch (weather) {
+        case "Sunny":
+          resolve("☀️");
+          break;
+        case "Cloudy":
+          resolve("☁️");
+          break;
+        case "Rainy":
+          resolve("🌧");
+          break;
+        default:
+          reject("Invalid Weather");
+      }
+    }, 1000);
+  });
+}
+
+function onSuccess(data) {
+  console.log(`Success: ${data}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+getWeather().then(getWeatherIcon).then(onSuccess, onError);
