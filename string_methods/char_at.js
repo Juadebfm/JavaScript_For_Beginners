78ced51841c1627d18a25d3f73b47f005e1d9a1f
Julius Adebowale <juadebgabriel@gmail.com>
2023-11-26 09:45:32 +0100

    fix

--- string_methods/char_at.js
+++ string_methods/char_at.js
@@ -1,5 +1,5 @@
 // charAt(): Takes index and it returns the value at that index
 let string = "PluralCode Javascript February Cohort Class";
-console.log(string.charAt(0)); // P
+console.log(string.charAt(0)); 
 let lastIndex = string.length - 1;
-console.log(string.charAt(lastIndex)); // s
+console.log(string.charAt(lastIndex)); 
